c6a656d4f9854d9ea8ccc44fb84fff3cec0a5d5a
Alex Story <astory11@yahoo.com>
2015-01-23 17:10:15 -0600

    initial commit

---
+++ sharpshapes/SharpShapes.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace SharpShapes{
+    public abstract class SharpShapes{
+
+      public Color BorderColor { set; get; }
+      public Color FillColor { set; get; }
+      public virtual int SidesCount { get; }
+      
+      abstract public  void scale(int percent);
+
+      abstract public decimal Area();
+
+      abstract public int Perimeter();
+  }    
+}
